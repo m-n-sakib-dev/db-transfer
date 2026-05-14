5fccc66366ed618643eed957853a2ad762408cfd
Nazmul Sakib <mdsakib.ndc17@gmail.com>
2026-05-14 16:50:14 +0600

    feat: integrate cron database for real-time process tracking and dynamic configuration updates

--- archive.py
+++ archive.py
@@ -5,8 +5,19 @@ import sys
 import logging
 from datetime import datetime
 import time
+import os
 
 # --- Configuration ---
+cron_db_config = {
+    'host': '104.248.157.66',
+    'port': 3306,     
+    'user': 'user_sakib',
+    'password': 'your_password',
+    'database': 'cron_db',
+    'ssl_ca': ""
+}
+
+
 source_config = {
     'host': '127.0.0.1',
     'port': 3306,     
@@ -57,47 +68,58 @@ target_shops = []
 table_not_check_shop_id = ['activity_log', 'sales']
 
 # --- Logging Setup ---
-logging.basicConfig(
-    filename='transfer_log.log',
-    filemode='w', 
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
+transfer_log = logging.getLogger('transfer_log')
+transfer_log.setLevel(logging.INFO)
+transfer_log_handler = logging.FileHandler('transfer_log.log',mode='w')
+transfer_log_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+transfer_log_handler.setFormatter(transfer_log_formatter)
+transfer_log.addHandler(transfer_log_handler)
 
-def connect_db():
+cron_log = logging.getLogger('cron_log')
+cron_log.setLevel(logging.INFO)
+cron_log_handler = logging.FileHandler('cron_log.log',mode='a')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+cron_log_handler.setFormatter(formatter)
+cron_log.addHandler(cron_log_handler)
+
+
+
+def connect_db(config):
     try:
-        src_conn = mysql.connector.connect(**source_config)
-        dest_conn = mysql.connector.connect(**dest_config)
-        return [src_conn,dest_conn]
+        conn = mysql.connector.connect(**config)
+        return conn
     except mysql.connector.Error as err:
-        logging.critical(f"Connection failed: {err}")
-        return [None,None]
+        transfer_log.critical(f"Failed to connect to {config['host']}:{config['database']} : {err}")
+        return None
         
-def disconnect_db(src_conn,dest_conn):
-    if src_conn: src_conn.close()
-    if dest_conn: dest_conn.close()
+def disconnect_db(conn):
+    if conn: conn.close()
 
 def sleep():
-    logging.info(f"Process is sleeping....")
+    transfer_log.info(f"Process is sleeping....")
     time.sleep(sleep_time)
-    logging.info(f"Process started again....")
+    transfer_log.info(f"Process started again....")
 
 def transfer_table_data(table_name):
-    logging.info(f"-----------------------------------------------------")
-    logging.info(f"-----------------------------------------------------")
+    pid = os.getpid()
+    transfer_log.info(f"-----------------------------------------------------")
+    transfer_log.info(f"-----------------------------------------------------")
     
-    logging.info(f"Starting transfer for table: {table_name}")
+    transfer_log.info(f"Starting transfer for table: {table_name}")
     src_conn = None
     dest_conn = None
     total_moved = 0
     total_deleted = 0
 
     try:
-        src_conn , dest_conn = connect_db() 
+        src_conn = connect_db(source_config)
+        dest_conn = connect_db(dest_config) 
+        cron_db_conn = connect_db(cron_db_config)
 
         src_cursor = src_conn.cursor(buffered=True)         # buffered=True allows us to keep the read cursor open during inserts
         dest_cursor = dest_conn.cursor()
         src_delete_cursor = src_conn.cursor()
+        cron_db_cursor = cron_db_conn.cursor(dictionary=True)
 
         params = [start_date, end_date]
 
@@ -125,13 +147,19 @@ def transfer_table_data(table_name):
             src_cursor.execute(f"SELECT * FROM {table_name} ")
             
         total_rows = src_cursor.rowcount
-        logging.info(f"START: Found {total_rows} rows in {table_name}")
+        transfer_log.info(f"START: Found {total_rows} rows in {table_name}")
+        cron_db_cursor.execute("REPLACE INTO process (scheduler_id, table_name, target_total_rows, status) SELECT s.id, %s, %s, %s FROM schedules s WHERE s.process_id = %s", (table_name, total_rows,'active', pid))
+        cron_db_conn.commit()
         column_names = [desc[0] for desc in src_cursor.description]     #getting column names of that table
         id_index = column_names.index('id') if 'id' in column_names else None       #getting the index to id column
 
         try:
         # 3. Batch Processing Loop
             while True:
+                cron_schedule_data = cron_db_cursor.execute("SELECT batch_size, sleep_time FROM schedules WHERE process_id = %s", (pid)).fetchone()
+                global batch_size,sleep_time
+                batch_size = cron_schedule_data['batch_size']
+                sleep_time = cron_schedule_data['sleep_time']
                 rows = src_cursor.fetchmany(batch_size)
                 if not rows:
                     break
@@ -143,8 +171,7 @@ def transfer_table_data(table_name):
                 dest_cursor.execute(f"SET FOREIGN_KEY_CHECKS = 0;")
                 dest_cursor.executemany(insert_sql, rows)                    
                 dest_cursor.execute(f"SET FOREIGN_KEY_CHECKS = 1;")
-                total_moved += len(rows)
-                logging.info(f"Progress: {total_moved}/{total_rows} moved from table {table_name}")
+                
                 
                 #deleting the moved rows
                 if delete_source_rows and id_index is not None and (table_name in archive_tables or table_name in order_dependent_archive_table):
@@ -156,27 +183,45 @@ def transfer_table_data(table_name):
                     deleted_rows = src_delete_cursor.rowcount
                     total_deleted += deleted_rows
                     src_delete_cursor.execute(f"SET FOREIGN_KEY_CHECKS = 1;")
-                    logging.info(f"Progress: {total_deleted}/{total_rows} deleted from source table {table_name}")
+                    transfer_log.info(f"Progress: {total_deleted}/{total_rows} deleted from source table {table_name}")
                 dest_conn.commit()    
                 src_conn.commit() 
+                total_moved += len(rows)
+                transfer_log.info(f"Progress: {total_moved}/{total_rows} moved from table {table_name}")
+                dest_cursor.execute(f"SELECT COUNT(*) FROM {table_name}")
+                final_dest_count = dest_cursor.fetchone()[0]
+                cron_db_cursor.execute("REPLACE INTO process (scheduler_id,table_name, converted_rows, dest_total_rows, status) SELECT s.id, %s, %s, %s, %s FROM schedules s WHERE s.process_id = %s", (table_name, total_moved, final_dest_count, 'processing', pid))
+                cron_db_conn.commit()
                 sleep()
         except mysql.connector.Error as e:
             dest_conn.rollback()
             dest_cursor.execute(f"SET FOREIGN_KEY_CHECKS = 1;")
             src_delete_cursor.execute(f"SET FOREIGN_KEY_CHECKS = 1;")
-            logging.error(f"Error moving batch near row {total_moved}: {e}")
+            transfer_log.error(f"Error moving batch near row {total_moved}: {e}")
+            cron_db_cursor.execute("REPLACE INTO process (scheduler_id,table_name, status) SELECT s.id, %s, %s FROM schedules s WHERE s.process_id = %s", (table_name,'error', pid))
+            cron_db_conn.commit()
 
         # 4. Post-transfer Verification
         dest_cursor.execute(f"SELECT COUNT(*) FROM {table_name}")
         final_dest_count = dest_cursor.fetchone()[0]
         
-        report = f"COMPLETE: Moved {total_moved} rows. Destination now total: {final_dest_count}"
-        logging.info(report)
+        if total_moved == total_deleted:
+            report = f"COMPLETE: Moved {total_moved} rows. Destination now total: {final_dest_count}"
+            transfer_log.info(report)
+            cron_db_cursor.execute("REPLACE INTO process (scheduler_id,table_name, converted_rows, dest_total_rows, status) SELECT s.id, %s, %s, %s, %s FROM schedules s WHERE s.process_id = %s", (table_name,total_moved, final_dest_count, 'completed', pid))
+            cron_db_conn.commit()
+
+        else:
+            transfer_log.error(f"ERROR: Moved {total_moved} rows. Deleted {total_deleted} rows. when target rows {total_rows} and destination rows {final_dest_count}")
+            cron_db_cursor.execute("REPLACE INTO process (scheduler_id,table_name, converted_rows, dest_total_rows, status) SELECT s.id, %s, %s, %s, %s FROM schedules s WHERE s.process_id = %s", (table_name,total_moved, final_dest_count, 'error', pid))
+            cron_db_conn.commit()
 
     except mysql.connector.Error as err:
-        logging.critical(f"Connection failed: {err}")
+        transfer_log.critical(f"Connection failed: {err}")
     finally:
-        disconnect_db(src_conn, dest_conn)
+        disconnect_db(src_conn)
+        disconnect_db(dest_conn)
+        disconnect_db(cron_db_conn)
 
         
         
@@ -184,7 +229,8 @@ def get_all_tables():
     src_conn = None
     dest_conn = None
     try:
-        src_conn , dest_conn = connect_db() 
+        src_conn = connect_db(source_config)
+        dest_conn = connect_db(dest_config) 
         src_cursor = src_conn.cursor(buffered=True)
         src_cursor.execute(f"SHOW TABLES;")
         rows = src_cursor.fetchall()
@@ -193,15 +239,23 @@ def get_all_tables():
         return tables_name
         
     except mysql.connector.Error as err:
-        logging.critical(f"Connection failed: {err}")
+        transfer_log.critical(f"Connection failed: {err}")
     finally:
-        disconnect_db(src_conn, dest_conn)        
+        disconnect_db(src_conn)
+        disconnect_db(dest_conn)        
         
         
 def transfer_data(target_tables):
+    pid = os.getpid()
     global delete_source_rows
     tables_name = []
     try:
+        cron_db_conn = connect_db(cron_db_config)
+        cron_cursor = cron_db_conn.cursor(dictionary=True)
+        cron_cursor.execute("SELECT p.table_name FROM process as p JOIN schedules as s ON p.scheduler_id=s.id WHERE s.process_id=%s AND p.status = 'completed'",(pid,))
+        completed_table_rows = cron_cursor.fetchall()
+        completed_tables = [row['table_name'] for row in completed_table_rows]
+
         print("data transfer initialized")
         if not target_tables:
             tables_name = get_all_tables()
@@ -209,22 +263,29 @@ def transfer_data(target_tables):
             tables_name = target_tables
         if 'job_batches' in tables_name:
             tables_name.remove('job_batches')
+        
         if 'orders' in tables_name:
             tables_name.remove('orders')
             delete_source_rows_temp = delete_source_rows
             try:
                 delete_source_rows = False
-                transfer_table_data('orders')
+                if 'orders' not in completed_tables:
+                    transfer_table_data('orders')
             finally:
                 delete_source_rows = delete_source_rows_temp
             tables_name.append('orders')
         for t_name in tables_name:
-            transfer_table_data(t_name) 
+            if t_name not in completed_tables:
+                transfer_table_data(t_name) 
+        
+        if 'orders' in tables_name:
+            transfer_table_data('orders')
         
         print("data transfer completed")
+        transfer_log.info("data transfer completed")
         
     except mysql.connector.Error as err:
-        logging.critical(f"Connection failed: {err}")
+        transfer_log.critical(f"Connection failed: {err}")
         
 
 def get_args():
@@ -263,7 +324,7 @@ def get_args():
     return parser.parse_args()
 
 
-def main():
+def main_old():
     args = get_args()
 
     source_config['host'] = args.src_host
@@ -284,7 +345,7 @@ def main():
     all_tables = get_all_tables()
     for table in tables_to_move:
         if table not in all_tables:
-            logging.error(f"Error: {table} does not exists in database.")
+            transfer_log.error(f"Error: {table} does not exists in database.")
             sys.exit(1)
         
     global target_shops
@@ -307,28 +368,29 @@ def main():
         start_date = datetime.strptime(args.start_date, "%Y-%m-%d")
         end_date = datetime.strptime(args.end_date, "%Y-%m-%d")
     except ValueError:
-        logging.error("Error: Dates must be in YYYY-MM-DD format.")
+        transfer_log.error("Error: Dates must be in YYYY-MM-DD format.")
         sys.exit(1)
         
     delete_source_rows = args.delete_source_rows
     delete_dest_rows = args.delete_source_rows
     
 
-    logging.info(f"source_config: {source_config}")
-    logging.info(f"dest_config: {dest_config}")
-    logging.info(f"tables_to_move: {tables_to_move}")
-    logging.info(f"shops_to_move: {target_shops }")
-    logging.info(f"start_date: {start_date}")
-    logging.info(f"end_date: {end_date}")
-    logging.info(f"delete_source_rows: {delete_source_rows}")
-    logging.info(f"delete_dest_rows: {delete_dest_rows}")
-    logging.info(f"batch_size: {batch_size}")
-    logging.info(f"sleep_time: {sleep_time}")
+    transfer_log.info(f"source_config: {source_config}")
+    transfer_log.info(f"dest_config: {dest_config}")
+    transfer_log.info(f"tables_to_move: {tables_to_move}")
+    transfer_log.info(f"shops_to_move: {target_shops }")
+    transfer_log.info(f"start_date: {start_date}")
+    transfer_log.info(f"end_date: {end_date}")
+    transfer_log.info(f"delete_source_rows: {delete_source_rows}")
+    transfer_log.info(f"delete_dest_rows: {delete_dest_rows}")
+    transfer_log.info(f"batch_size: {batch_size}")
+    transfer_log.info(f"sleep_time: {sleep_time}")
 
 
-    src,dst=connect_db()      
+    src= connect_db(source_config)
+    dst=connect_db(dest_config)      
     if src and dst:
-        logging.info("database connected")
+        transfer_log.info("database connected")
         print("database connected")
         if test_connection:
             print("test connection successful")
@@ -336,17 +398,144 @@ def main():
             return
         transfer_data(tables_to_move)
     if not src:
-        logging.error("source could not be connect")
+        transfer_log.error("source could not be connect")
         print("source could not be connect")  
     if not dst:
-        logging.error("dest could not be connect")
+        transfer_log.error("dest could not be connect")
         print("dest could not be connect")
     
 
+def start_process(job):
+    args = job
+
+    source_config['host'] = args['src_host']
+    source_config['port'] = int(args['src_port']) if args['src_port'] else 3306
+    source_config['user'] = args['src_user_name']
+    source_config['password'] = args['src_pass']
+    source_config['database'] = args['src_db_name']
+    source_config['ssl_ca'] = args['src_ca_cert']
+
+    dest_config['host'] = args['dest_host']
+    dest_config['port'] = int(args['dest_port']) if args['dest_port'] else 3306
+    dest_config['user'] = args['dest_user_name']
+    dest_config['password'] = args['dest_pass']
+    dest_config['database'] = args['dest_db_name']
+    dest_config['ssl_ca'] = args['dest_ca_cert']
+
+        
+    global target_shops
+    global start_date
+    global end_date
+    global delete_source_rows 
+    global delete_dest_rows 
+    global batch_size
+    global sleep_time
+
+    batch_size = args['batch_size']
+    sleep_time = args['sleep_time']
+    test_connection = args['test_connection']
+    start_date = args['start_date']
+    end_date = args['end_date']
+        
+    delete_source_rows = args['delete_source_rows']
+    delete_dest_rows = args['delete_source_rows']
+    
+
+    transfer_log.info(f"source_config: {source_config}")
+    transfer_log.info(f"dest_config: {dest_config}")
+    transfer_log.info(f"start_date: {start_date}")
+    transfer_log.info(f"end_date: {end_date}")
+    transfer_log.info(f"delete_source_rows: {delete_source_rows}")
+    transfer_log.info(f"delete_dest_rows: {delete_dest_rows}")
+    transfer_log.info(f"batch_size: {batch_size}")
+    transfer_log.info(f"sleep_time: {sleep_time}")
+
+
+    src= connect_db(source_config)
+    dst=connect_db(dest_config)      
+    if src and dst:
+        transfer_log.info("database connected")
+        print("database connected")
+        if test_connection:
+            print("test connection successful")
+            transfer_log.info("test connection successful")
+            sleep()
+            return
+        else:
+            transfer_data()
+    if not src:
+        transfer_log.error("source could not be connect")
+        print("source could not be connect")  
+    if not dst:
+        transfer_log.error("dest could not be connect")
+        print("dest could not be connect")
+
+    
+
+def main():
+    global transfer_log, transfer_log_handler, transfer_log_formatter
+    try:
+        cron_db = connect_db(cron_db_config)   
+        if cron_db:
+            cron_cursor = cron_db.cursor(dictionary=True)
+            cron_cursor.execute(
+                    "SELECT id, process_id, src_host, src_port, src_db_name, src_user_name, "
+                    "src_pass, src_ca_cert, dest_host, dest_port, dest_db_name, dest_user_name, "
+                    "dest_pass, dest_ca_cert, start_time, batch_size, sleep_time, status, "
+                    "created_at, updated_at, start_date, end_date, delete_source_rows, test_connection "
+                    "FROM schedules WHERE status <> 'completed' LIMIT 1"
+                )
+
+            scheduled_job = cron_cursor.fetchone()
+
+
+            if scheduled_job:
+                if not scheduled_job['process_id']:
+                    pid = os.getpid()
+                    print("pid",pid)
+                    cron_cursor.execute(
+                        "UPDATE schedules SET process_id = %s, status = 'running' WHERE id = %s",
+                        (pid, scheduled_job['id'])
+                    )
+                    cron_db.commit()
+                    start_process(scheduled_job)  
+                else :
+                    try:
+                        os.kill(scheduled_job['process_id'], 0) 
+                        print("Job is already running")
+                        cron_log.info("Job is already running")
+                        return 
+                    except OSError:
+
+                        cron_log.warning("Previous job was terminated without completion , new job will start now.")
+                        print("Previous job was terminated without completion , new job will start now.")
+                        
+                        transfer_log.removeHandler(transfer_log_handler)
+                        transfer_log_handler = logging.FileHandler('transfer_log.log',mode='a')
+                        transfer_log_handler.setFormatter(transfer_log_formatter)
+                        transfer_log.addHandler(transfer_log_handler)
+
+                        pid = os.getpid()
+                        cron_cursor.execute(
+                            "UPDATE schedules SET process_id = %s, status = 'running' WHERE id = %s",
+                            (pid, scheduled_job['id'])
+                        )
+                        cron_db.commit()
+                        start_process(scheduled_job)  
+            else:
+                print("No scheduled job found")
+                cron_log.info("No scheduled job found to execute")
+            
+    except Exception as e:
+        cron_log.error(f"Error: {e}")
+        print(f"Error: {e}")
+    finally:
+        if cron_db:
+            disconnect_db(cron_db)
 
 
 if __name__ == '__main__':
     main()
     print("Process finished")
-    logging.info("Process finished")
+    cron_log.info("Process finished")
     
